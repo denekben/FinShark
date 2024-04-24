1ec0b528c9a0d2e7b1b2d08d6364006b10ca2ca4
Denis Kurbanaev <denekben1@yandex.ru>
2024-04-24 23:09:02 +0300

    test

--- api/api/obj/Debug/net8.0/api.MvcApplicationPartsAssemblyInfo.cs
+++ api/api/obj/Debug/net8.0/api.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ae8fe0dd1a4421420f9035fd42b799306c96bd")]
-[assembly: System.Reflection.AssemblyProductAttribute("api")]
-[assembly: System.Reflection.AssemblyTitleAttribute("api")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
